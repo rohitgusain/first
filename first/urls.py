9dd9fafca46a997d78dfc2649568925a25847784
rohit gusain <rohitgusain@DESKTOP-NV59N7P.localdomain>
2019-02-17 17:16:39 +0530

    changed homepage to home

--- first/urls.py
+++ first/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('count', views.count),url('about', views.index),url('contact', views.contact),url('', views.homepage, name = 'home'),
+    url('count', views.count),url('about', views.index),url('contact', views.contact),url('', views.home, name = 'home'),
 ]
